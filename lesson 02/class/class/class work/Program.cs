110a3d6b62fc1c49ed0895dcb493de34aaca8444
Jodda1991 <Jodda1991@gmail.com>
2019-07-17 22:15:56 +0300

    Second lesson added (with homework)

---
+++ lesson 02/class/class/class work/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace class_work
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            a = 10;
+
+            int b = 20;
+
+            string name = "Sum of";
+
+            char symbol = '=';
+
+            Console.WriteLine($"{name} {a} + {b} {symbol} {a+b}");
+
+            byte maximumByte = 255;
+
+            sbyte maximumSignedByte = SByte.MaxValue;
+            sbyte minimumSignedByte = SByte.MinValue;
+
+            byte age = 36;
+
+            byte ageInHex = 0x24; // 36
+
+            short minimumShort = short.MinValue;
+            short maximumShort = short.MaxValue;
+
+            int maximumInt = int.MaxValue;
+            int minimumInt = int.MinValue;
+
+            double s = 1.7E+3;
+
+            Console.WriteLine($"[{minimumSignedByte} ... {maximumSignedByte}], {maximumByte}, {ageInHex} = {age}, [{minimumShort}...{maximumShort}], [{minimumInt}...{maximumInt}], {s}");
+
+            int sun = 1_321_123;
+            long universe = sun;
+            sun = (int)universe;
+            float f = sun;
+            sun = (int)f;
+            string valueOfSun = sun.ToString();
+            Console.WriteLine($"{valueOfSun}, {sun}");
+
+            Console.WriteLine("Введите число");
+            string value = Console.ReadLine();
+            int integerValue = int.Parse(value);
+            Console.WriteLine($"{value + value}, {integerValue + integerValue}");
+        }
+    }
+}
---
+++ lesson 02/home/nomework/nomework/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace nomework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите два числа");
+            Console.WriteLine("Ввод первого числа:");
+            int firstNumber = int.Parse(Console.ReadLine()); 
+            Console.WriteLine("Ввод второго числа:");
+            int secondNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine($"умножение: {firstNumber * secondNumber}, сложение: {firstNumber + secondNumber}");
+        } 
+    }
+}
