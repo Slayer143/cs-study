60b07a9320a9698be50ac883b87a147641fe5a9b
Nikita <niki2025@mail.ru>
2019-08-23 15:23:48 +0300

    homework update

--- lesson 06/home/homework 06 part 2/homework 06 part 2/Program.cs
+++ lesson 06/home/homework 06 part 2/homework 06 part 2/Program.cs
@@ -6,18 +6,35 @@ namespace homework_06_part_2
     {
         static void Main(string[] args)
         {
-            bool incorrectInput = true;
+            double process, desiredAmount, downAmount = 0;
+            int daysCounter = 1;
+            double percent = 0;
 
-            while (incorrectInput)
+            try
             {
-                try
-                {
+                Console.WriteLine("Enter down payment amount");
+                downAmount = Convert.ToDouble(Console.ReadLine());
 
-                }
-                catch ()
-                {
+                Console.WriteLine("Enter percentage of income (1% = 0.01)");
+                percent = Convert.ToDouble(Console.ReadLine());
 
+                Console.WriteLine("Enter desired amount of accumulation");
+                desiredAmount = Convert.ToDouble(Console.ReadLine());
+
+                process = downAmount + downAmount * percent;
+
+                while (process < desiredAmount)
+                {
+                    process = process + process * percent;
+                    daysCounter++;
                 }
+
+                Console.WriteLine($"Desired amount of accumulation {desiredAmount} will be achieved after {daysCounter} days.");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                throw e;
             }
         }
     }
---
+++ lesson 07/home/homework 07 part 01/homework 07 part 01/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace homework_07_part_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool process = true;
+            int wordsCounter = 0;
+
+            while (process)
+            {
+                Console.WriteLine("Enter string that contains more that one word");
+                try
+                {
+                    string inputString = Console.ReadLine();
+
+                    for (int i = 0; i < inputString.Length; i++)
+                    {
+                        if (string.IsNullOrWhiteSpace(inputString) == false 
+                            && inputString[i] == ' ')
+                        {
+                            wordsCounter++;
+
+                            if (inputString[i] == ' ' 
+                                && inputString[i++] == ' ')
+                            {
+                                wordsCounter--;
+                            }
+                        }
+
+                    }
+
+                    if (wordsCounter >= 1) process = false;
+                    else Console.WriteLine("You should input more than one word");
+                }
+                catch
+                {
+
+                }
+            }
+        }
+    }
+}
