05868209b73ecd90d0f55e4f9a52cb7579c4cbae
basalduk <47829188+basalduk@users.noreply.github.com>
2019-09-02 19:25:21 +0300

    with lesson 10 class

---
+++ lesson 10/class/class 10 task/class 10 task/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace class_10_task
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var pet1 = new Pet();
+			pet1.PetKind = Pet.Kind.Dog;
+			pet1.Age = 1;
+			pet1.Name = "Jonh";
+			pet1.Sex = 'M';
+			pet1.SetBirthPlace("London");
+
+
+			Console.WriteLine(pet1.Description);
+
+			var pet2 = new Pet()
+			{
+				PetKind = Pet.Kind.Cat,
+				Age = 5,
+				Name = "Julie",
+				Sex = 'F',
+			};
+			pet2.SetBirthPlace("Krit");
+
+			Console.WriteLine(pet2.Description);
+		}
+	}
+
+	class Pet
+	{
+		public enum Kind
+		{
+			Mouse,
+			Cat,
+			Dog
+		};
+
+		public Kind PetKind;
+		private string _birthPlace;
+		public string Name;
+		private char _sex;
+
+		public char Sex
+		{
+			get
+			{
+				return _sex;
+			}
+			set
+			{
+				if (char.ToLowerInvariant(value).Equals('f') || char.ToLowerInvariant(value).Equals('m'))
+					_sex = char.ToUpperInvariant(value);
+				else throw new Exception("Wrong data");
+			}
+		}
+		public int Age { get; set; }
+		public string Description
+		{
+			get
+			{
+				return $"{Name} is a {PetKind} ({_sex}) of {Age} years old from {_birthPlace}";
+			}
+		}
+
+		public void SetBirthPlace(string place)
+		{
+			_birthPlace = place;
+		}
+	}
+}
---
+++ lesson 10/class/class 10/class 10/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+class AloneClass
+{
+
+	public class SubClass
+	{
+		public static void PrintFromSubClass()
+		{
+			Console.WriteLine("Hello from SubClass!");
+		}
+	}
+
+	public static void PrintWorld()
+	{
+		Console.WriteLine("World!");
+	}
+}
+
+namespace class_10
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			AnotherNamespace.MyDemoClass.PrintHello();
+			AloneClass.PrintWorld();
+			AloneClass.SubClass.PrintFromSubClass();
+		}
+	}
+}
+
+namespace AnotherNamespace
+{
+
+	class MyDemoClass
+	{
+
+		public static void PrintHello()
+		{
+			Console.WriteLine("Hello!");
+		}
+	}
+}
