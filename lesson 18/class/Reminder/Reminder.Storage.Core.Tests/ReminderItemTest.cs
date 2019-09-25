c9fae232f29a54b583bb452f0bf34f3b94506225
ledzs <leksakov01@ya.ru>
2019-09-25 22:27:40 +0300

    lesson 18

---
+++ lesson 18/class/Reminder/Reminder.Storage.Core.Tests/ReminderItemTest.cs
@@ -0,0 +1,48 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Reminder.Storage.Core.Tests
+{
+	[TestClass]
+	public class ReminderItemTest
+	{
+		[TestMethod]
+		public void Constructor_Parameters_Should_Fill_Properties_Correctly()
+		{
+			// prepare test data
+			DateTimeOffset date = DateTimeOffset.Parse("2010-01-01T00:00:00");
+			const string contactId = "0123ABC",
+				message = "testMessage";
+
+			// do test
+			var reminderItem = new ReminderItem(
+				date,
+				message,
+				contactId);
+
+			// check result
+			Assert.IsNotNull(reminderItem);
+			Assert.AreEqual(date, reminderItem.Date);
+			Assert.AreEqual(message, reminderItem.Message);
+			Assert.AreEqual(contactId, reminderItem.ContactId);
+		}
+
+		[TestMethod]
+		public void Constructor_Should_Fill_Id_Other_That_Guid_Empty()
+		{
+			// prepare test data
+			DateTimeOffset date = DateTimeOffset.Parse("2010-01-01T00:00:00");
+			const string contactId = "0123ABC",
+				message = "testMessage";
+
+			// do test
+			var reminderItem = new ReminderItem(
+				date,
+				message,
+				contactId);
+
+			// check result
+			Assert.AreEqual(Guid.Empty, reminderItem.Id);
+		}
+	}
+}
---
+++ lesson 18/class/Reminder/Reminder.Storage.InMemory/InMemoryreminderStorage.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using Reminder.Storage.Core;
+using System.Linq;
+
+namespace Reminder.Storage.InMemory
+{
+	public class InMemoryReminderStorage : IReminderStorage
+	{
+		private Dictionary<Guid, ReminderItem> reminders;
+
+		public void Add(ReminderItem reminderItem)
+		{
+			reminders.Add(reminderItem.Id, reminderItem);
+		}
+
+		public ReminderItem Get(Guid id)
+		{
+			return reminders.ContainsKey(id)
+				? reminders[id]
+				: null;
+		}
+
+		public List<ReminderItem> Get(ReminderItemStatus status)
+		{
+			return reminders
+				.Values
+				.Where((ReminderItem ri) => ri.Status == status)
+				.ToList();
+		}
+
+		public void Update(Guid id, ReminderItemStatus status)
+		{
+			if (reminders.ContainsKey(id))
+				reminders[id].Status = status;
+		}
+	}
+}
