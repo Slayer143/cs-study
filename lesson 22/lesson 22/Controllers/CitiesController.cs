a7a8c2fbdec990ea30c32350ef5f0eda0cf4fc10
ledzs <leksakov01@ya.ru>
2019-10-09 22:41:40 +0300

    lesson 22

---
+++ lesson 22/lesson 22/Controllers/CitiesController.cs
@@ -0,0 +1,45 @@
+﻿using lesson_22.DataStore;
+using lesson_22.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace lesson_22.Controllers
+{
+	public class CitiesController : Controller
+	{
+		[HttpGet("/api/cities")]
+		public IActionResult GetCities()
+		{
+			var citiesDataStore = CitiesDataStore.GetInstance();
+			var cities = citiesDataStore.Cities;
+			return Ok(cities);
+		}
+
+		[HttpGet("/api/cities/{id}")]
+		public IActionResult GetCity(int id)
+		{
+			var citiesDataStore = CitiesDataStore.GetInstance();
+
+			var city = citiesDataStore
+				.Cities
+				.FirstOrDefault(c => c.Id == id);
+
+			if (city == null)
+				return NotFound();
+
+			return Ok(city);
+		}
+
+		[HttpPost("/api/cities/")]
+		public IActionResult AddCity([FromBody] City city)
+		{
+			var citiesDataStore = CitiesDataStore.GetInstance();
+			citiesDataStore.Cities.Add(city);
+
+			return Created("/api/cities/" + city.Id, city);
+		}
+	}
+}
---
+++ lesson 22/lesson 22/DataStore/CitiesDataStore.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using lesson_22.Models;
+
+namespace lesson_22.DataStore
+{
+	public class CitiesDataStore
+	{
+		private static CitiesDataStore _citiesDataStore;
+
+		public List<City> Cities { get; }
+
+		private CitiesDataStore()
+		{
+			Cities = new List<City>
+			{
+				new City
+				{
+					Id = 1,
+					Name = "New-York",
+					Description = "This is my favorite city",
+					NumberOfPointsOfInterest = 100
+				},
+				new City
+				{
+					Id = 2,
+					Name = "Amsterdam",
+					Description = "I ❤ Amster",
+					NumberOfPointsOfInterest = 99
+				}
+			};
+		}
+
+		public static CitiesDataStore GetInstance()
+		{
+			if (_citiesDataStore == null)
+				_citiesDataStore = new CitiesDataStore();
+
+			return _citiesDataStore;
+		}
+	}
+}
